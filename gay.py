cf0729e35bc658a8bc2444cebe9e5a9cc1b6544e
phuocle1231236-sys <phuocle1231236@gmail.com>
2026-04-21 10:26:00 +0700

    git commit git commit -m

--- gay.py
+++ gay.py
@@ -2,18 +2,17 @@ import numpy as np
 import pandas as pd
 from scipy import stats
 covid_data = pd.read_csv('covid_data.csv')
-covid_data=covid_data[['iso_code','continent','location','date','total_cases','new_cases']]
+covid_data=covid_data[['Entity','Code','Day','Central estimate']]
 covid_data.head(5)
 covid_data.dtypes
-covid_data.describe()
 covid_data.shape
-data_mean=np.mean(covid_data['new_cases'])
-data_median=np.median(covid_data['new_cases'])
-data_mode=stats.mode(covid_data['new_cases'])
-data_variance=np.var(covid_data['new_cases'])
-data_sd=np.std(covid_data['new_cases'])
-data_max=np.max(covid_data['new_cases'])
-data_min=np.min(covid_data['new_cases'])
-data_percentiles=np.percentile(covid_data['new_cases'],60)
-data_quartiles=np.percentile(covid_data['new_cases'],0.75)
-data_IQR=stats.iqr(covid_data['new_cases'])
+data_mean=np.mean(covid_data['Central estimate'])
+data_median=np.median(covid_data['Central estimate'])
+data_mode=stats.mode(covid_data['Central estimate'])
+data_variance=np.var(covid_data['Central estimate'])
+data_sd=np.std(covid_data['Central estimate'])
+data_max=np.max(covid_data['Central estimate'])
+data_min=np.min(covid_data['Central estimate'])
+data_percentiles=np.percentile(covid_data['Central estimate'],60)
+data_quartiles=np.percentile(covid_data['Central estimate'],0.75)
+data_IQR=stats.iqr(covid_data['Central estimate'])
